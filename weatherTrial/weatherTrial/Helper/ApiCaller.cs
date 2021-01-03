2084a476ab2d26caeb640997c3e4f0baa7ef042a
James Joseph Meldrum <meldrum.james@hotmail.com>
2021-01-03 05:04:36 +0000

    learning how to call the API, also created a model for the weather info

---
+++ weatherTrial/weatherTrial/Helper/ApiCaller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weatherTrial.Helper
+{
+    public class ApiCaller
+    {
+        public static async Task<ApiResponse> Get(string url, string authId = null)
+        {
+             using (var client = new HttpClient())
+            {
+                if (!string.IsNullOrWhiteSpace(authId))
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Authorisation", authId);
+
+                var request = await client.GetAsync(url);
+                if (request.IsSuccessStatusCode)
+                {
+                    return new ApiResponse { Response = await request.Content.ReadAsStringAsync() };
+                }
+                else
+                    return new ApiResponse { ErrorMessage = request.ReasonPhrase };
+            }
+        }
+    }
+
+    public class ApiResponse
+    {
+        public bool Successful => ErrorMessage == null;
+        public string ErrorMessage { get; set; }
+        public string Response { get; set; } 
+    }
+}
--- weatherTrial/weatherTrial/Views/CurrentWeather.xaml.cs
+++ weatherTrial/weatherTrial/Views/CurrentWeather.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using weatherTrial.Helper;
+using weatherTrial.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +17,32 @@ namespace weatherTrial.Views
         public CurrentWeather()
         {
             InitializeComponent();
+        }
+
+        private string Location = "France";
+        private static string Api = "95748421f66c31fa90cc554df977162e";
+
+        private async void GetWeatherInfo()
+        {
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&appid={Api}";
+
+            var result = await ApiCaller.Get(url);
+
+            if (result.Successful)
+            {
+                try
+                {
+                    var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(result.Response);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            else
+            {
+                await DisplayAlert("Weather Info", "No Weather information found", "OK");
+            }
         }
     }
 }
