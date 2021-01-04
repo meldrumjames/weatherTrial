d15c8f38d79de54c4604c491b2c129b980ccd2e5
James Joseph Meldrum <meldrum.james@hotmail.com>
2021-01-04 00:21:44 +0000

    got the main part to display the current weather working., been trying to get the 4 day forecast to show but not working properly.

--- weatherTrial/weatherTrial/Views/CurrentWeather.xaml.cs
+++ weatherTrial/weatherTrial/Views/CurrentWeather.xaml.cs
@@ -17,14 +17,15 @@ namespace weatherTrial.Views
         public CurrentWeather()
         {
             InitializeComponent();
+            GetWeatherInfo();
         }
 
-        private string Location = "France";
+        private string Location = "London";
         private static string Api = "95748421f66c31fa90cc554df977162e";
 
         private async void GetWeatherInfo()
         {
-            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&appid={Api}";
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&appid={Api}&units=metric";
 
             var result = await ApiCaller.Get(url);
 
@@ -33,6 +34,17 @@ namespace weatherTrial.Views
                 try
                 {
                     var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(result.Response);
+                    descriptionTxt.Text = weatherInfo.weather[0].description.ToUpper();
+                    iconImg.Source = $"w{weatherInfo.weather[0].icon}";
+                    cityTxt.Text = weatherInfo.name.ToUpper();
+                    temperatureTxt.Text = weatherInfo.main.temp.ToString("0");
+                    humidityTxt.Text = $"{weatherInfo.main.humidity}%";
+                    pressureTxt.Text = $"{weatherInfo.main.pressure} hpa";
+                    windTxt.Text = $"{weatherInfo.wind.speed} m/s";
+                    cloudinessTxt.Text = $"{weatherInfo.clouds.all}%";
+
+                    var dt = new DateTime(1970, 01, 01).ToUniversalTime().AddSeconds(weatherInfo.dt);
+                    dateTxt.Text = dt.ToString("dddd, MMM dd").ToUpper();
                 }
                 catch (Exception ex)
                 {
@@ -42,6 +54,59 @@ namespace weatherTrial.Views
             else
             {
                 await DisplayAlert("Weather Info", "No Weather information found", "OK");
+            }
+        }
+
+        private async void GetForecast()
+        {
+            var url = $"http://api.openweathermap.org/data/2.5/forecast?q={Location}&appid={Api}&units=metric";
+            var result = await ApiCaller.Get(url);
+
+            if (result.Successful)
+            {
+                try
+                {
+                    var forecastInfo = JsonConvert.DeserializeObject<ForecastInfo>(result.Response);
+
+                    List<List> allList = new List<List>();
+
+                    foreach (var list in forecastInfo.list)
+                    {
+                        var date = DateTime.Parse(list.dt_txt);
+                        //var date = new DateTime(1970, 01, 01).
+
+                        if (date > DateTime.Now && date.Hour == 0 && date.Minute == 0 && date.Second == 0)
+                            allList.Add(list);
+                    }
+
+                    dayOneTxt.Text = DateTime.Parse(allList[0].dt_txt).ToString("dddd");
+                    dateOneTxt.Text = DateTime.Parse(allList[0].dt_txt).ToString("dd MMM");
+                    iconOneImg.Source = $"w{allList[0].weather[0].icon}";
+                    tempOneTxt.Text = allList[0].main.temp.ToString("0");
+
+                    dayTwoTxt.Text = DateTime.Parse(allList[1].dt_txt).ToString("dddd");
+                    dateTwoTxt.Text = DateTime.Parse(allList[1].dt_txt).ToString("dd MMM");
+                    iconTwoImg.Source = $"w{allList[1].weather[0].icon}";
+                    tempTwoTxt.Text = allList[1].main.temp.ToString("0");
+
+                    dayThreeTxt.Text = DateTime.Parse(allList[2].dt_txt).ToString("dddd");
+                    dateThreeTxt.Text = DateTime.Parse(allList[2].dt_txt).ToString("dd MMM");
+                    iconThreeImg.Source = $"w{allList[2].weather[0].icon}";
+                    tempThreeTxt.Text = allList[2].main.temp.ToString("0");
+
+                    dayThreeTxt.Text = DateTime.Parse(allList[3].dt_txt).ToString("dddd");
+                    dateThreeTxt.Text = DateTime.Parse(allList[3].dt_txt).ToString("dd MMM");
+                    iconThreeImg.Source = $"w{allList[3].weather[0].icon}";
+                    tempThreeTxt.Text = allList[3].main.temp.ToString("0");
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Weather Info", ex.Message, "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Weathter Info", "No forecast information found", "OK");
             }
         }
     }
